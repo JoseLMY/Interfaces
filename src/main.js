48a98bee4438e4da74044ea76d0955753e0cfa34
Jose Luis <jose.lmy2004@gmail.com>
2023-03-03 17:02:41 -0500

    Agregué nuevas cosita.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ bSettingsContainer.addEventListener("click", viewSettings)
 const messagesContainer = document.querySelector(".messagesContainer")
 const details = document.querySelector(".details")
 details.addEventListener("click", viewMessages)
+const bViewFormReport = document.querySelector(".bViewFormReport")
+bViewFormReport.addEventListener("click", viewFormReport)
+const formContainer = document.querySelector(".formContainer")
+const exitReportData = document.querySelector(".exitReportData")
+exitReportData.addEventListener("click", fExitReportData)
+const bSendReport = document.querySelector(".bSendReport")
+bSendReport.addEventListener("click", fExitReportData)
 
 
 function viewSettings (){
@@ -20,6 +27,23 @@ function viewMessages (){
             config.classList.add("view")
     } else {
         messagesContainer.classList.add("view")
+    }
+}
 
+function viewFormReport(){
+    if (formContainer.classList.contains("view")){
+        formContainer.classList.remove("view")
+        bViewFormReport.classList.add("view")
+    } else {
+        formContainer.classList.add("view")
+    }
+}
+function fExitReportData(){
+    if (bViewFormReport.classList.contains("view")){
+        formContainer.classList.add("view")
+        bViewFormReport.classList.remove("view")
+    } else {
+        bViewFormReport.classList.remove("view")
+        formContainer.classList.add("view")
     }
 }
