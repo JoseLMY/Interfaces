cf6a62cbf23398fa772feace03efc5faeacedc31
Jose Luis <jose.lmy2004@gmail.com>
2023-03-03 18:24:10 -0500

    Terminé de agregar el cuadro de texto donde confirma que se envio exitosamente el reporte.

--- src/main.js
+++ src/main.js
@@ -10,8 +10,11 @@ const formContainer = document.querySelector(".formContainer")
 const exitReportData = document.querySelector(".exitReportData")
 exitReportData.addEventListener("click", fExitReportData)
 const bSendReport = document.querySelector(".bSendReport")
-bSendReport.addEventListener("click", fExitReportData)
-
+bSendReport.addEventListener("click", fSendedReport)
+const reportSended = document.querySelector(".reportSended")
+const bReportSended = document.querySelector(".bReportSended")
+bReportSended.addEventListener("click", finishReport)
+const reportSendedContainer = document.querySelector(".reportSendedContainer")
 
 function viewSettings (){
     if (config.classList.contains("view")){
@@ -34,6 +37,7 @@ function viewFormReport(){
     if (formContainer.classList.contains("view")){
         formContainer.classList.remove("view")
         bViewFormReport.classList.add("view")
+        config.classList.add("view")
     } else {
         formContainer.classList.add("view")
     }
@@ -41,9 +45,26 @@ function viewFormReport(){
 function fExitReportData(){
     if (bViewFormReport.classList.contains("view")){
         formContainer.classList.add("view")
+        reportSended.classList.remove("view")
         bViewFormReport.classList.remove("view")
     } else {
-        bViewFormReport.classList.remove("view")
+        formContainer.classList.remove("view")
+        reportSended.classList.add("view")
+    }
+}
+function fSendedReport (){
+    if (formContainer.classList.contains("view")){
+        formContainer.classList.remove("view")
+    } else {
+        reportSendedContainer.classList.remove("view")
         formContainer.classList.add("view")
+    }
+}
+function finishReport(){
+    if (reportSended.classList.contains("view")){
+        reportSended.classList.remove("view")
+    } else {
+        bViewFormReport.classList.remove("view")
+        reportSendedContainer.classList.add("view")
     }
 }
