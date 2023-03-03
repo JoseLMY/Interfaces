2922cceaeba8f7eb3920468e2bc4959a15d28c25
Jose Luis <jose.lmy2004@gmail.com>
2023-03-03 12:31:46 -0500

    Modificamos algunos aspectos para que al momento de usar no se corran algunas secciones.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 const config = document.querySelector(".config")
 const bSettingsContainer = document.querySelector(".bSettingsContainer")
-bSettingsContainer.addEventListener("click", toggleItems)
+bSettingsContainer.addEventListener("click", viewSettings)
+const messagesContainer = document.querySelector(".messagesContainer")
+const details = document.querySelector(".details")
+details.addEventListener("click", viewMessages)
 
 
-function toggleItems (){
+function viewSettings (){
     if (config.classList.contains("view")){
             config.classList.remove("view")
+            messagesContainer.classList.add("view")
     } else {
         config.classList.add("view")
+    }
+}
+function viewMessages (){
+    if (messagesContainer.classList.contains("view")){
+            messagesContainer.classList.remove("view")
+            config.classList.add("view")
+    } else {
+        messagesContainer.classList.add("view")
+
     }
 }
