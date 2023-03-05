3a0ace4b17ca3df88c34821b896d5ed75ae34865
Jose Luis <jose.lmy2004@gmail.com>
2023-03-05 15:20:59 -0500

    Se terminó de crear la interfaz para que se puedan mandar mensajes a otros usuarios.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-const config = document.querySelector(".config")
-const bSettingsContainer = document.querySelector(".bSettingsContainer")
+const config = document.querySelector(".config"),  bSettingsContainer = document.querySelector(".bSettingsContainer")
+// const bSettingsContainer = document.querySelector(".bSettingsContainer")
 bSettingsContainer.addEventListener("click", viewSettings)
 const messagesContainer = document.querySelector(".messagesContainer")
 const details = document.querySelector(".details")
